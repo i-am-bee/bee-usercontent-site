a17e5a61798a6f3e7e2314efaea2cf1bf1989e97
Jan Pokorný <JenomPokorny@gmail.com>
2024-12-10 10:22:40 +0100

    feat: update run.py, switch to embedded streamlit, add request-response mechanism (#9)
    * feat: update run.py
    
    Signed-off-by: Jan Pokorný <jenompokorny@gmail.com>
    
    * feat: switch to bundled Stlite
    
    Signed-off-by: Jan Pokorný <jenompokorny@gmail.com>
    
    * feat: patch stlite, switch to native postMessage
    
    Signed-off-by: Jan Pokorný <jenompokorny@gmail.com>
    
    * feat: switch to async mode
    
    Signed-off-by: Jan Pokorný <jenompokorny@gmail.com>
    
    * fix: typos
    
    Co-authored-by: Radek Ježek <pc.jezek@gmail.com>
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>
    
    * small fixes
    
    Signed-off-by: Radek Ježek <radek.jezek@ibm.com>
    
    * feat: make "Try to fix this error" button configurable
    
    Signed-off-by: Jan Pokorný <jenompokorny@gmail.com>
    
    * fix: give normal names to requests
    
    Signed-off-by: Jan Pokorný <jenompokorny@gmail.com>
    
    ---------
    
    Signed-off-by: Jan Pokorný <jenompokorny@gmail.com>
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>
    Signed-off-by: Radek Ježek <radek.jezek@ibm.com>
    Co-authored-by: Radek Ježek <pc.jezek@gmail.com>
    Co-authored-by: Radek Ježek <radek.jezek@ibm.com>

--- src/entry.ts
+++ src/entry.ts
@@ -1,9 +1,7 @@
+import runPy from './python/run.py?raw';
+
 declare global {
   const stlite: any;
-
-  interface Window {
-    app: any;
-  }
 }
 
 // Page is loaded directly
@@ -12,6 +10,25 @@ if (window === window.parent) window.location.href = 'https://ibm.com';
 const ALLOWED_ORIGINS = (import.meta.env.VITE_ALLOWED_FRAME_ANCESTORS ?? '').split(' ').filter(Boolean);
 
 (() => {
+  const app = stlite.mount(
+    {
+      requirements: ['requests', 'pydantic'],
+      entrypoint: 'trigger.py',
+      files: {
+        'trigger.py': 'import run; await run.run()',
+        'app.py': '',
+        'config.json': '{}',
+        'run.py': runPy,
+      },
+      streamlitConfig: {
+        'client.toolbarMode': 'minimal',
+        'server.runOnSave': true,
+        'theme.primaryColor': '#0f62fe',
+      },
+    },
+    document.getElementById('root'),
+  );
+
   window.addEventListener('message', async (event) => {
     const { data, origin } = event;
 
@@ -26,47 +43,42 @@ const ALLOWED_ORIGINS = (import.meta.env.VITE_ALLOWED_FRAME_ANCESTORS ?? '').spl
     const { classList } = document.body;
 
     switch (data.type) {
-      case 'setFullscreen':
+      case 'bee:setFullscreen':
         classList.toggle('fullscreen', data.value);
 
         return;
-      case 'updateTheme':
+      case 'bee:updateTheme':
         classList.remove(data.theme === 'light' ? 'cds--g90' : 'cds--white');
         classList.add(data.theme === 'light' ? 'cds--white' : 'cds--g90');
 
         return;
-      case 'updateCode':
-        await window.app.writeFile('app.py', data.code);
-        await window.app.writeFile('trigger.py', 'import run; await run.run(); # ' + Math.random());
+      case 'bee:updateCode':
+        await app.writeFile('app.py', data.code);
+        await app.writeFile('config.json', JSON.stringify(data.config ?? {}));
+        await app.writeFile('trigger.py', 'import run; await run.run(); # ' + Math.random());
 
         return;
-      case 'reportError':
-        ALLOWED_ORIGINS.forEach((origin: string) =>
-          parent.postMessage({ type: 'reportError', errorText: data?.errorText }, origin),
-        );
+      case 'bee:response':
+        app.kernel._worker.postMessage(data);
         return;
       default:
         return;
     }
   });
 
-  window.app = stlite.mount(
-    {
-      requirements: ['requests'],
-      entrypoint: 'trigger.py',
-      files: {
-        'trigger.py': 'import run; await run.run()',
-        'app.py': '',
-        // NOTE: in production, we probably want to inline these two
-        'run.py': { url: 'python/run.py' },
-        'util.py': { url: 'python/util.py' },
-      },
-      streamlitConfig: {
-        'client.toolbarMode': 'minimal',
-        'server.runOnSave': true,
-        'theme.primaryColor': '#0f62fe',
-      },
-    },
-    document.getElementById('root'),
-  );
+  app.kernel._worker.addEventListener('message', (event: MessageEvent) => {
+    const data = typeof event.data === 'string' ? JSON.parse(event.data) : event.data;
+    switch (data.type) {
+      case 'bee:reportError':
+        ALLOWED_ORIGINS.forEach((origin: string) =>
+          parent.postMessage({ type: data.type, errorText: data?.errorText }, origin),
+        );
+        return;
+      case 'bee:request':
+        ALLOWED_ORIGINS.forEach((origin: string) => parent.postMessage(data, origin));
+        return;
+      default:
+        return;
+    }
+  });
 })();
